914af08c6a323df41723ae1e50c9867ed126e011
Adan Marques <adan__marx@hotmail.com>
2024-11-16 19:45:29 -0300

    feat(p2-pdm): inclui botao na tela

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, FlatList } from 'react-native';
+//import catKey from '../../a/key.js';
+
+
+
+/* const endPoint = "https://api.thecatapi.com/v1/images/search?api_key=";
+const apiKey = catKey;
+const obtemImagens = () => {
+  const target = endPoint + apiKey;
+  fetch(target)
+    .then((dados) => {
+    });
+} */
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable
+        style={styles.button}
+        onPress={alert("a")}>
+        <Text
+          style={styles.buttonText}>
+          Gerar fotos de gatos
+        </Text>
+      </Pressable>
+      {/* <FlatList
+         keyExtractor={item => item.id!}
+         //style={styles.list}
+         data={lembretes}
+         renderItem={l => (
+           <View
+             style={styles.listItem}>
+           </View>
+         )}
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +44,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    //justifyContent: 'center',
+  },
+  button: {
+    width: '50%',
+    backgroundColor: '#0096F3',
+    padding: 12,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold'
   },
 });
