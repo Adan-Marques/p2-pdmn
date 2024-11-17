5a1f18566f13b3c99a72ad7e8f35032798103c35
Adan Marques <adan__marx@hotmail.com>
2024-11-16 21:22:12 -0300

    feat(p2-pdm): inclui endpoint de TheCatAPI

--- App.js
+++ App.js
@@ -1,38 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, FlatList } from 'react-native';
-//import catKey from '../../a/key.js';
-
-
-
-/* const endPoint = "https://api.thecatapi.com/v1/images/search?api_key=";
-const apiKey = catKey;
-const obtemImagens = () => {
-  const target = endPoint + apiKey;
-  fetch(target)
-    .then((dados) => {
-    });
-} */
+import { StyleSheet, Text, View, Pressable, FlatList, Image } from 'react-native';
+import catKey from './key.js';
+import { useState } from 'react';
 
 export default function App() {
+  const [gato, setGatos] = useState([])
+
+  const endPoint = "https://api.thecatapi.com/v1/images/search?api_key=";
+  const apiKey = catKey;
+
+  const obtemImagens = () => {
+    const target = endPoint + apiKey;
+    fetch(target)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let imagesData = data;
+        imagesData.map(function (imageData) {
+          let image = `${imageData.url}`;
+          console.log(image)
+        });
+      })
+  }
+
+
   return (
     <View style={styles.container}>
       <Pressable
         style={styles.button}
-        onPress={alert("a")}>
+        onPress={() => obtemImagens()}>
         <Text
           style={styles.buttonText}>
           Gerar fotos de gatos
         </Text>
       </Pressable>
+      <View>
+        <Text>Deverá aparecer aqui embaixo</Text>
+        <Image
+          source={{ uri: gato.url }}
+        />
+      </View>
       {/* <FlatList
-         keyExtractor={item => item.id!}
-         //style={styles.list}
-         data={lembretes}
-         renderItem={l => (
-           <View
-             style={styles.listItem}>
-           </View>
-         )}
+        //keyExtractor={item => item.url}
+        //style={styles.list}
+        //data={gato}
+        renderItem={i => (
+          <View>
+            <Image
+              source={{ uri: target }}
+            />
+          </View>
+        )}
       /> */}
       <StatusBar style="auto" />
     </View>
